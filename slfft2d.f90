54da8a75b36a014bd8bc7a2bede3a93efdf16385
Alexandre Poux <work@alexandrepoux.fr>
2016-04-27 14:45:58 +0200

    some cleaning

--- slfft2d.f90
+++ slfft2d.f90
@@ -16,80 +16,69 @@ USE param
 
 integer, intent(in) :: nx,ny,mx,my,nxm,nym,nwork,ntrigsX,ntrigsY
 integer, intent(in) :: isign
-real(8),  intent(inout), dimension(mx,my) :: x
-real(8),  dimension(mx,my) :: x5
 real(8), dimension(nwork) :: work,table
+real(8),  intent(inout), dimension(mx,my) :: x
 real(8), dimension(mx,my) :: x1,x2
-real(8), dimension(nx,ny) :: x3,x4
-integer :: inc, jump
 integer :: i, j
 
-  complex(C_DOUBLE_COMPLEX) :: fftw_cmp_x(nx),fftw_cmp_y(ny)
-  real(C_double) :: fftw_real_x(nx),fftw_real_y(ny)
+  complex(C_DOUBLE_COMPLEX) :: fftw_cmp_x(nx,ny),fftw_cmp_y(ny,nx)
   type(C_PTR) :: fftw_plan_x1,fftw_plan_y,fftw_plan_x2
 
 
 !************************FFT2D***(0-0)***********************************
     
       fftw_plan_x1 = fftw_plan_dft_r2c_1d(nx, &
-                                    fftw_real_x, fftw_cmp_x, &
+                                    x(1:nx,1), fftw_cmp_x(1:nx,1), &
                                     FFTW_ESTIMATE)
                                     
       fftw_plan_x2 = fftw_plan_dft_c2r_1d(nx, &
-                                    fftw_cmp_x, fftw_real_x, &
+                                    fftw_cmp_x(1:nx,1), x(1:nx,1), &
                                     FFTW_ESTIMATE)
                                     
      fftw_plan_y = fftw_plan_dft_1d(ny,   &
-                                fftw_cmp_y, fftw_cmp_y,&
+                                fftw_cmp_y(1:ny,1), fftw_cmp_y(1:ny,1),&
                                 isign, FFTW_ESTIMATE);
 
    if (isign==-1) then
       ! On applique une TF reelle->complexe aux colonnes de ce tableau
       do j=1,ny
-        fftw_real_x=x(1:nx,j)
-        call fftw_execute_dft_r2c(fftw_plan_x1, fftw_real_x, fftw_cmp_x)
-        x3(:,j)=real(fftw_cmp_x)/nym/2
-        x4(:,j)=aimag(fftw_cmp_x)/nym/2
+        call fftw_execute_dft_r2c(fftw_plan_x1, x(1:nx,j), fftw_cmp_x(1:nx,j))
       enddo
+      
+      !renormalisation
+      fftw_cmp_x=fftw_cmp_x/nym/2
+      fftw_cmp_y=transpose(fftw_cmp_x)
       
       ! On applique une TF complexe->complexe aux lignes de ce tableau
       do i=1,nx
-        fftw_cmp_y=cmplx(x3(i,1:ny),x4(i,1:ny))
-        call fftw_execute_dft(fftw_plan_y, fftw_cmp_y, fftw_cmp_y)
-        x3(i,:)=real(fftw_cmp_y)
-        x4(i,:)=aimag(fftw_cmp_y)
+        call fftw_execute_dft(fftw_plan_y, fftw_cmp_y(1:ny,i), fftw_cmp_y(1:ny,i))
       enddo        
       
       do j=1,ny
       do i=1,mx/2
-         x(2*i-1,j)=x3(i,j)
-         x(2*i,j)=x4(i,j)
+         x(2*i-1,j)=real(fftw_cmp_y(j,i))/nym
+         x(2*i,j)=aimag(fftw_cmp_y(j,i))/nym
       enddo
       enddo
-      x(:,:)=x(:,:)/nym
    endif
    
    if (isign==1) then
       ! On applique une TF complexe->complexe aux lignes de ce tableau
-      do j=1,my
+      do j=1,ny
       do i=1,mx/2
-         x1(i,j)=x(2*i-1,j)
-         x2(i,j)=x(2*i,j)
+         fftw_cmp_y(j,i)=cmplx(x(2*i-1,j),x(2*i,j))
       enddo
       enddo
       
       do i=1,nx
-        fftw_cmp_y=cmplx(x1(i,1:ny),x2(i,1:ny))
-        call fftw_execute_dft(fftw_plan_y, fftw_cmp_y, fftw_cmp_y)
-        x3(i,:)=real(fftw_cmp_y)
-        x4(i,:)=aimag(fftw_cmp_y)
+        call fftw_execute_dft(fftw_plan_y, fftw_cmp_y(1:ny,i), fftw_cmp_y(1:ny,i))
       enddo      
+
+      fftw_cmp_x=transpose(fftw_cmp_y)
 
       ! On applique une TF reelle<-complexe aux colonnes de ce tableau
       do j=1,ny
-        fftw_cmp_x=cmplx(x3(1:nx,j),x4(1:nx,j))
-        call fftw_execute_dft_c2r(fftw_plan_x2, fftw_cmp_x, fftw_real_x)
-        x(1:nx,j)=fftw_real_x
+        call fftw_execute_dft_c2r(fftw_plan_x2, fftw_cmp_x(1:nx,j), x(1:nx,j))
       enddo
       
    endif
